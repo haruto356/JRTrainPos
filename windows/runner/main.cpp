9abc839bafbd1fa69936e0ca20cf4b08f92b5e64
haruto356 <harutoharu327@gmail.com>
2025-11-13 14:17:27 +0900

    update: アプリ名を変更

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.Create(L"jr_train_pos", origin, size)) {
+  if (!window.Create(L"\u975e\u516c\u5f0fJRW\u5217\u8eca\u4f4d\u7f6e", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
