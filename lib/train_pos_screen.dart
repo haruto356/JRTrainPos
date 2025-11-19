70e773d4378c761fb4e1ee5c9b34cfa80140ccf2
haruto356 <harutoharu327@gmail.com>
2025-11-20 07:33:06 +0900

    refactor: StatefulWidgetを別ファイルに移動

--- lib/train_pos_screen.dart
+++ lib/train_pos_screen.dart
@@ -3,6 +3,7 @@ import 'package:jr_train_pos/file_operation.dart';
 import 'dart:convert';
 
 import 'package:jr_train_pos/get_json_file.dart';
+import 'package:jr_train_pos/widgets/train.dart';
 
 class TrainPosScreen extends StatefulWidget {
   const TrainPosScreen({super.key, required this.lineName, required this.lineColor, required this.lineCodeColor});
@@ -189,50 +190,6 @@ class _TrainPosScreenState extends State<TrainPosScreen> {
             )
           ],
         ),
-      ),
-    );
-  }
-}
-
-class Train extends StatefulWidget {
-  const Train({super.key, required this.lineColor, required this.posFirst, required this.posSecond, required this.direction});
-
-  final int lineColor;
-  final int posFirst;
-  final int posSecond;
-  final int direction;
-
-  @override
-  State<Train> createState() => _TrainState();
-}
-
-class _TrainState extends State<Train> {
-  int _posTop = 0;
-  late double screenWidth;
-
-  @override
-  void initState() {
-    super.initState();
-
-    // 停車中
-    if(widget.posSecond == 0) {
-      _posTop = widget.posFirst;
-    }
-    // 駅間
-    else {
-      _posTop = widget.posFirst - 1;
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Positioned(
-      top: _posTop * 70 + 40,
-      left: widget.direction == 0 ? MediaQuery.of(context).size.width / 2 - 75 : MediaQuery.of(context).size.width / 2 + 55,
-      child: Container(
-        height: 30,
-        width: 30,
-        color: Color(widget.lineColor),
       ),
     );
   }
