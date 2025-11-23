01e15b57641db07b0ca9399bcb88eaf04b691366
haruto356 <harutoharu327@gmail.com>
2025-11-24 07:48:35 +0900

    update: 列車の画像を変更

--- lib/widgets/train.dart
+++ lib/widgets/train.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class Train extends StatefulWidget {
   const Train({super.key, required this.lineColor, required this.trainMap, required this.stationList, required this.stationPosMap});
 
@@ -18,7 +19,6 @@ class _TrainState extends State<Train> {
   @override
   void initState() {
     super.initState();
-    print(widget.trainMap);
 
     _direction = int.parse(widget.trainMap['direction']!);
 
@@ -39,7 +39,7 @@ class _TrainState extends State<Train> {
   Widget build(BuildContext context) {
     return Positioned(
       top: _posTop * 70 + 40,
-      left: _direction == 0 ? MediaQuery.of(context).size.width / 2 - 75 : MediaQuery.of(context).size.width / 2 + 55,
+      left: _direction == 0 ? MediaQuery.of(context).size.width / 2 - 85 : MediaQuery.of(context).size.width / 2 + 45,
       child: InkWell(
         onTap: (){
           // 下から列車情報画面を表示
@@ -61,10 +61,14 @@ class _TrainState extends State<Train> {
             );
           });
         },
-        child: Container(
-          height: 30,
-          width: 30,
-          color: Color(widget.lineColor),
+        child: Transform.scale(
+          scaleY: _direction == 0 ? 1 : -1,
+          child: Image(
+            image: AssetImage('assets/images/train.png'),
+            height: 45,
+            width: 45,
+            color: Color(widget.lineColor),
+          ),
         ),
       ),
     );
