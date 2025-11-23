df7252c1f80f37e945f07efb5703ab513b37c4c6
haruto356 <harutoharu327@gmail.com>
2025-11-23 20:55:48 +0900

    update: 列車を選択すると下から詳細画面が出るように変更

--- lib/widgets/train.dart
+++ lib/widgets/train.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 class Train extends StatefulWidget {
   const Train({super.key, required this.lineColor, required this.trainMap, required this.stationList, required this.stationPosMap});
 
@@ -19,6 +18,7 @@ class _TrainState extends State<Train> {
   @override
   void initState() {
     super.initState();
+    print(widget.trainMap);
 
     _direction = int.parse(widget.trainMap['direction']!);
 
@@ -40,10 +40,32 @@ class _TrainState extends State<Train> {
     return Positioned(
       top: _posTop * 70 + 40,
       left: _direction == 0 ? MediaQuery.of(context).size.width / 2 - 75 : MediaQuery.of(context).size.width / 2 + 55,
-      child: Container(
-        height: 30,
-        width: 30,
-        color: Color(widget.lineColor),
+      child: InkWell(
+        onTap: (){
+          // 下から列車情報画面を表示
+          showModalBottomSheet(context: context, builder: (BuildContext context){
+            return Column(
+              children: [
+                Container(
+                  height: 50,
+                  color: Color(widget.lineColor),
+                  child: Row(
+                    children: [
+                      Text(widget.trainMap['displayType']!),
+                      Spacer(),
+                      widget.trainMap['numberOfCars'] == null ? Text('') : Text('${widget.trainMap['numberOfCars']!}両'),
+                    ],
+                  ),
+                ),
+              ],
+            );
+          });
+        },
+        child: Container(
+          height: 30,
+          width: 30,
+          color: Color(widget.lineColor),
+        ),
       ),
     );
   }
