1f3cb0f6fb81f8b2ee1db22858235c783ad04579
haruto356 <harutoharu327@gmail.com>
2025-12-29 10:57:29 +0900

    add: 設定ページに仮のライセンスページを追加

--- lib/setting_screen.dart
+++ lib/setting_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 class SettingScreen extends StatefulWidget {
   const SettingScreen({super.key});
@@ -8,8 +9,54 @@ class SettingScreen extends StatefulWidget {
 }
 
 class _SettingScreenState extends State<SettingScreen> {
+  String appVersion = "";
+
+  @override
+  void initState() {
+    super.initState();
+
+    Future(() async {
+      PackageInfo packageInfo = await PackageInfo.fromPlatform();
+
+      appVersion = packageInfo.version;
+
+      setState(() {});
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('設定')));
+    if(appVersion.isEmpty){
+      return Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('設定'),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => LicensePage(
+                      applicationName: '非公式JRW列車位置',
+                      applicationVersion: appVersion,
+                    ),
+                  ),
+                );
+              },
+              child: const Text('ライセンス'),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
