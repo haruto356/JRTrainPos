f3e637c64027d679bf3cd8767ea997a09a55c18b
haruto356 <harutoharu327@gmail.com>
2026-03-12 10:43:59 +0900

    update: JR公式の遅延情報へ遷移するボタンを追加

--- lib/screens/my_line_screen.dart
+++ lib/screens/my_line_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:jr_train_pos/core/line_manager.dart';
 import 'package:jr_train_pos/screens/my_line_edit_screen.dart';
 import 'package:jr_train_pos/core/shared_pref.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MyLineScreen extends StatefulWidget {
   const MyLineScreen({super.key});
@@ -18,6 +19,7 @@ class _MyLineScreenState extends State<MyLineScreen> {
   bool _isGetMyLineList = false;
 
   late FloatingActionButton _floatingActionButton;
+  late Widget _goJrOfficialInfoButton;
 
   // SharedPreferencesからデータを取得する関数
   Future<void> _getMyLineList() async{
@@ -29,8 +31,6 @@ class _MyLineScreenState extends State<MyLineScreen> {
 
   @override
   void initState() {
-    super.initState();
-
     // my路線編集ボタン
     _floatingActionButton = FloatingActionButton(
       onPressed: () async {
@@ -39,6 +39,26 @@ class _MyLineScreenState extends State<MyLineScreen> {
       },
       child: const Icon(Icons.edit),
     );
+
+    // JR公式情報遷移ボタン
+    _goJrOfficialInfoButton = Ink(
+      decoration: BoxDecoration(
+        color: Colors.blue[100],
+        border: Border.all(color: Colors.blue),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: InkWell(
+        onTap: (){
+          launchUrl(Uri.parse('https://trafficinfo.westjr.co.jp/kinki.html'));
+        },
+        child: const Padding(
+          padding: EdgeInsetsGeometry.fromLTRB(35, 3, 35, 3),
+          child: Text('JR西日本公式遅延情報'),
+        ),
+      )
+    );
+
+    super.initState();
 
     Future(() async{
       await _getMyLineList();
@@ -58,8 +78,15 @@ class _MyLineScreenState extends State<MyLineScreen> {
     if(_myLineList.isEmpty){
       return Scaffold(
         floatingActionButton: _floatingActionButton,
-        body: const Center(
-          child: Text('My路線が登録されていません'),
+        body: Center(
+          child: Column(
+            children: [
+              const SizedBox(height: 15,),
+              _goJrOfficialInfoButton,
+              const SizedBox(height: 10,),
+              const Text('My路線が登録されていません'),
+            ],
+          )
         ),
       );
     }
@@ -71,6 +98,8 @@ class _MyLineScreenState extends State<MyLineScreen> {
         body: SingleChildScrollView(
           child: Column(
             children: [
+              const SizedBox(height: 15,),
+              _goJrOfficialInfoButton,
               const Padding(
                 padding: EdgeInsets.all(12),
                 child: Text('My路線', style: TextStyle(fontSize: 16),),
